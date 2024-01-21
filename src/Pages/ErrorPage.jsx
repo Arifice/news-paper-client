f7367ce689b175baf9fe089624695cba5c93389a
Ariful Islam <arifice13@gmail.com>
2024-01-21 22:04:08 +0600

    router setup , error page setup are done

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,18 @@
+
+import { Link } from 'react-router-dom';
+import logo from '../assets/error/error.jpg'
+const ErrorPage = () => {
+    
+    return (
+        <div className=''>
+            <div className='flex justify-center my-20'>
+             <img src={logo} alt="" />
+            </div>
+            <div className='flex justify-center'>
+                <Link to={'/'}><button className='btn btn-lg btn-warning  text-3xl'>Go Home</button></Link>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
